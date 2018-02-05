672bb3b2cb1a64e7e24f045c8ddaeb7c24631337
Evgeniy Akulich <zhenya-akulich@mail.ru>
2018-02-05 15:15:59 +0300

    Create Infrustructure folder and Configurated Ninject

---
+++ SimpleShop.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+
+
+namespace SimpleShop.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+
+        }
+
+
+
+
+
+
+    } // end NinjectDependencyResolver()
+
+} // end namespace
