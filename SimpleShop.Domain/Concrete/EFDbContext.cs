de0df487bb87102aeba1177bbabff35f0c684eaf
Evgeniy Akulich <zhenya-akulich@mail.ru>
2018-02-06 11:07:20 +0300

    Added EF Context + connection string to database

---
+++ SimpleShop.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using SimpleShop.Domain.Entities;
+
+namespace SimpleShop.Domain.Concrete
+{
+    class EFDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+
+    } // end class
+
+} // end namespase
