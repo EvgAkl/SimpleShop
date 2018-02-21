49d501fd87a0b8cd49b15415e9a03f442c7084e0
Evgeniy Akulich <zhenya-akulich@mail.ru>
2018-02-21 12:29:54 +0300

    Added  CartController with methods  of actions + model for view Index + views Index and GameSummary

---
+++ SimpleShop.WebUI/Controllers/CartController.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SimpleShop.Domain.Entities;
+using SimpleShop.Domain.Abstract;
+using SimpleShop.WebUI.Models;
+
+namespace SimpleShop.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        // Declaring variable
+        private IGameRepository repository;
+        // Declaring constructors
+        public CartController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+        // Declaring methods
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        } // end Index()
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        } // end GetCart()
+
+        public RedirectToRouteResult AddToCart(int Id, string returnUrl)
+        {
+            Game game = repository.Games.FirstOrDefault(s => s.Id == Id);
+
+            if (game != null)
+            {
+                GetCart().AddItem(game, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        } // end AddToCart()
+
+        public RedirectToRouteResult RemoveFromCart(int Id, string returnUrl)
+        {
+            Game game = repository.Games.FirstOrDefault(s => s.Id == Id);
+
+            if (game != null)
+            {
+                GetCart().RemoveLine(game);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        } // end RemoveFromCart()
+
+    } // end controller
+
+} // end namrspace
---
+++ SimpleShop.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SimpleShop.Domain.Entities;
+
+namespace SimpleShop.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+
+    } // end class
+
+} // end namespace
