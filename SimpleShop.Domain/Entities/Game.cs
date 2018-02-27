2790617f161ac59a76f604c2c1cd504859e8e278
Evgeniy Akulich <zhenya-akulich@mail.ru>
2018-02-27 14:52:03 +0300

    (+) "Edit" view (Modify) "Game" Entity

--- SimpleShop.Domain/Entities/Game.cs
+++ SimpleShop.Domain/Entities/Game.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace SimpleShop.Domain.Entities
 {
     public class Game
-    {
+    {   
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+        [Display(Name = "Название")]
         public string Name { get; set; }
+        [Display(Name = "Описание")]
+        [DataType(DataType.MultilineText)]
         public  string Description { get; set; }
+        [Display(Name = "Категория")]
         public string Category { get; set; }
+        [Display(Name = "Цена (руб)")]
         public decimal Price { get; set; }
     } // end class
 
