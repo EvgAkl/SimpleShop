e6db5e7a50c4e6ff57a55ca96643d53e776c2003
Evgeniy Akulich <zhenya-akulich@mail.ru>
2018-02-28 12:06:41 +0300

    Validate message = (Modify) "Game" entity, "Edit" view

--- SimpleShop.Domain/Entities/Game.cs
+++ SimpleShop.Domain/Entities/Game.cs
@@ -8,13 +8,18 @@ namespace SimpleShop.Domain.Entities
         [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
         [Display(Name = "Название")]
+        [Required(ErrorMessage = "Пожалуйста, введите название игры")]
         public string Name { get; set; }
         [Display(Name = "Описание")]
+        [Required(ErrorMessage = "Пожалуйста, введите описание игры")]
         [DataType(DataType.MultilineText)]
         public  string Description { get; set; }
         [Display(Name = "Категория")]
+        [Required(ErrorMessage = "Пожалуйста, укажите категорию для игры")]
         public string Category { get; set; }
         [Display(Name = "Цена (руб)")]
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите положительное значение для цены")]
         public decimal Price { get; set; }
     } // end class
 
