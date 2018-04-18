6e8af82160b65625999db577b187ab9fd8e30116
Evgeniy Akulich <zhenya-akulich@mail.ru>
2018-04-18 12:46:43 +0300

    (+) LoginViewModel class, Account controller and action method "Login" in it

---
+++ SimpleShop.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using SimpleShop.WebUI.Models;
+using SimpleShop.WebUI.Infrastructure.Abstract;
+
+
+namespace SimpleShop.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        IAuthProvider authProvider;
+        public AccountController(IAuthProvider auth)
+        {
+            authProvider = auth;
+        } // end constructor
+
+        public ViewResult login()
+        {
+            return View();
+        } // end login()
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (authProvider.Authenticate(model.UserName, model.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин или пароль");
+                    return View();
+                }
+            } // end main if
+            else
+            {
+                return View();
+            }
+
+        } // end Index()
+
+    } // end class
+
+} // end namespace
---
+++ SimpleShop.WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleShop.WebUI.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    } // end class
+
+} // end namespace
