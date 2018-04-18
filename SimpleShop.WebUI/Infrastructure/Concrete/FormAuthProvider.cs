acc67978ea6fb7ed96680fec7d41a5fa83866f61
Evgeniy Akulich <zhenya-akulich@mail.ru>
2018-04-18 12:15:20 +0300

    (+) "Concreate" folder, "FormAuthProvider" class

---
+++ SimpleShop.WebUI/Infrastructure/Concrete/FormAuthProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Security;
+using SimpleShop.WebUI.Infrastructure.Abstract;
+
+namespace SimpleShop.WebUI.Infrastructure.Concrete
+{
+    public class FormAuthProvider : IAuthProvider
+    {
+        public bool Authenticate(string username, string password)
+        {
+            bool result = FormsAuthentication.Authenticate(username, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+            }
+            return result;
+        } // end Authenticate()
+
+
+    } // end class
+
+} // end namespace
